0491264447d1358ed79385734990734dbe542bbf
sewemark <sewerynjakub.markowicz@gmail.com>
2017-05-30 19:30:04 +0200

    Reading database file added, DatabaseContentParser interface added

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -28,12 +28,67 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.Text = "Form1";
+            this.menuStrip2 = new System.Windows.Forms.MenuStrip();
+            this.readDatabaseToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.readFromFile = new System.Windows.Forms.ToolStripMenuItem();
+            this.selectDatabaseFileDialog = new System.Windows.Forms.OpenFileDialog();
+            this.menuStrip2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // menuStrip2
+            // 
+            this.menuStrip2.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.readDatabaseToolStripMenuItem});
+            this.menuStrip2.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip2.Name = "menuStrip2";
+            this.menuStrip2.Size = new System.Drawing.Size(284, 24);
+            this.menuStrip2.TabIndex = 0;
+            this.menuStrip2.Text = "menuStrip2";
+            // 
+            // readDatabaseToolStripMenuItem
+            // 
+            this.readDatabaseToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.readFromFile});
+            this.readDatabaseToolStripMenuItem.Name = "readDatabaseToolStripMenuItem";
+            this.readDatabaseToolStripMenuItem.Size = new System.Drawing.Size(96, 20);
+            this.readDatabaseToolStripMenuItem.Text = "Read Database";
+            // 
+            // readFromFile
+            // 
+            this.readFromFile.Name = "readFromFile";
+            this.readFromFile.Size = new System.Drawing.Size(152, 22);
+            this.readFromFile.Text = "From file";
+            this.readFromFile.Click += new System.EventHandler(this.ReadFromFileButtonClick);
+            // 
+            // selectDatabaseFileDialog
+            // 
+            this.selectDatabaseFileDialog.FileName = "databaseFilePath";
+            this.selectDatabaseFileDialog.Filter = "Text|*.txt;";
+            // 
+            // Form1
+            // 
+            this.ClientSize = new System.Drawing.Size(284, 261);
+            this.Controls.Add(this.menuStrip2);
+            this.MainMenuStrip = this.menuStrip2;
+            this.Name = "Form1";
+            this.menuStrip2.ResumeLayout(false);
+            this.menuStrip2.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem readToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem readFromFileButton;
+        private System.Windows.Forms.ToolStripMenuItem dsdToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem dsdsToolStripMenuItem;
+        private System.Windows.Forms.MenuStrip menuStrip2;
+        private System.Windows.Forms.ToolStripMenuItem readDatabaseToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem readFromFile;
+        private System.Windows.Forms.OpenFileDialog selectDatabaseFileDialog;
     }
 }
 
--- Form1.cs
+++ Form1.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -12,9 +13,33 @@ namespace OakStatisticalAnalysis
 {
     public partial class Form1 : Form
     {
+        string[] databaseLines;
         public Form1()
         {
             InitializeComponent();
         }
+
+        private void ReadFromFileButtonClick(object sender, EventArgs e)
+        {
+            GetDatabaseFilePath();
+        }
+
+        private void GetDatabaseFilePath()
+        {
+            DialogResult result = selectDatabaseFileDialog.ShowDialog();
+            if (result == DialogResult.OK)
+            {
+                string file = selectDatabaseFileDialog.FileName;
+                try
+                {
+                     databaseLines = File.ReadAllLines(file);
+                }
+                catch (IOException ioException)
+                {
+                    MessageBox.Show(ioException.Message.ToString());
+                }
+            }
+        }
+        
     }
 }
