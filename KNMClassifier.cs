e3f972cc12817111e55e62fcb3fcae02ccca862a
sewemark <sewerynjakub.markowicz@gmail.com>
2017-06-17 12:07:22 +0200

    KNM classifier added

---
+++ KNMClassifier.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using OakStatisticalAnalysis.Models;
+
+namespace OakStatisticalAnalysis
+{
+    internal class KNMClassifier : IClassifier
+    {
+        private List<Sample> trainingSet;
+        public void Classify()
+        {
+        }
+
+        public List<Sample> GetTrainingSet()
+        {
+            return trainingSet;
+        }
+
+        public void Train(List<Sample> _trainingSet)
+        {
+            trainingSet = _trainingSet;
+        }
+    }
+}
